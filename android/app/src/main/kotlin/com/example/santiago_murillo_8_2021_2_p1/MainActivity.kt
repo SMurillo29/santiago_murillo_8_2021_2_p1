ffafbd85b6611af400b8b4a7fbce9707dff8335a
Santiago Murillo Vergara <exsssmurillo@serviciosnutresa.onmicrosoft.com>
2024-04-07 14:44:56 -0500

    update android folder

--- android/app/src/main/kotlin/com/example/santiago_murillo_8_2021_2_p1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/santiago_murillo_8_2021_2_p1/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.santiago_murillo_8_2021_2_p1
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
